be4b657be887a9b9930ac6ec3a45baa1c9e93ce6
Haowei <mukgis@126.com>
2022-04-11 10:18:42 +0800

    Update generate_hfp.js

--- generate_hfp.js
+++ generate_hfp.js
@@ -31,8 +31,8 @@ Map.addLayer(Pop2009_score,{},'Pop2009_score')
 var nt2009 = ee.Image('users/lixuecaosysu/globalNTL/stepwise_cal_DMSP/F162009v4b_webstable_lightsavg_vis')
 var nt2009 = nt2009.updateMask(nt2009.gte(6))
 var nt2009_score = ee.Image(0)
-          .where(nt2009.gte(6).and(nt2009.lt(7)), 1)
-          .where(nt2009.gte(7).and(nt2009.lt(7)), 2)
+          .where(nt2009.gte(5).and(nt2009.lt(6)), 1)
+          .where(nt2009.gte(6).and(nt2009.lt(7)), 2)
           .where(nt2009.gte(7).and(nt2009.lt(8)), 3)
           .where(nt2009.gte(8).and(nt2009.lt(9)), 4)
           .where(nt2009.gte(9).and(nt2009.lt(11)), 5)
@@ -130,4 +130,4 @@ Export.image.toAsset({
   maxPixels:1e13
 });
 
-// https://code.earthengine.google.com/d5b2b49a672cb83b1380515cd0b935d1?noload=1
+// https://code.earthengine.google.com/a3037df913b465faa599b52365be94be?noload=1
